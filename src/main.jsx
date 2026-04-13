2ba387bccaac9973a5bd87ca438edacc25ec35a6
bigexperiment <ganesha.727@gmail.com>
2026-04-12 21:41:05 -0400

    Initial React Vite arrows game setup

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ArrowsGame from "../arrows-game.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ArrowsGame />
+  </React.StrictMode>,
+);
